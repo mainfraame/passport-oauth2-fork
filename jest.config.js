979df17dc8845d98d360bc288088aeb64a94162c
dayne.mentier <dayne.mentier@vestwell.com>
2022-06-19 20:09:04 -0400

    refactor: convert to typescript
    * convert all files to typescript
    * rename lib folder to src
    * switch from mocha to jest for unit testing
    * add prettier configuration
    * switch jshint to eslint
    * convert all prototype structures to classes
    * rename files to match exports
    * update tests to jest
    * add depcheck
    * remove makefile
    * update travis config
    * update gitignore to exclude dist
    * add rollup-based build system using preppy
    * update chai and chai passport

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.ts'],
   coverageProvider: 'v8',
   coverageReporters: ['text', 'lcov'],
-  forceExit: true,
   moduleFileExtensions: ['ts', 'js', 'json'],
   testMatch: ['<rootDir>/test/**/*.spec.ts'],
   testEnvironment: 'node'
